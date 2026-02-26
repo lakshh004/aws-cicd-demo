7a7441cf9c1df69aad683d148cd9f6d59881197f
Lakshya Purohit <lakshyapurohit@Lakshyas-MacBook-Air.local>
2026-02-27 05:12:37 +0530

    Test deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("CI/CD Pipeline Working 🚀");
+  res.send("If you're seeing this then the CI/CD Pipeline is Successful 🚀");
 });
 
 app.listen(3000, () => {
